5fd36f2d8d6080a816a820643cb2834ea25aeb9a
AushaL <thetingler85@gmail.com>
2024-05-24 19:02:27 +0300

    feat: add language select mobile functionality

--- source/js/components/language-select.js
+++ source/js/components/language-select.js
@@ -1,32 +1,81 @@
 export default function languageSelect() {
-  const languageButton = document.querySelector(".header__language-button");
-  const languageOptions = document.querySelectorAll(".header__language-option");
+  const languageButton = document.querySelector(
+    ".header__language-button--desktop"
+  );
 
-  function openSelect() {
-    languageButton.classList.add("active");
+  const languageOptions = languageButton.querySelectorAll(
+    ".header__language-option"
+  );
 
-    languageOptions.forEach((option) => {
-      option.classList.add("active");
-    });
+  const languageButtonMobile = document.querySelector(
+    ".header__language-button--mobile"
+  );
+
+  const languageOptionsMobile = languageButtonMobile.querySelectorAll(
+    ".header__language-option"
+  );
+
+  function openSelect(isMobile) {
+    if (isMobile) {
+      languageButtonMobile.classList.add("active");
+
+      languageOptionsMobile.forEach((option) => {
+        option.classList.add("active");
+      });
+    } else {
+      languageButton.classList.add("active");
+
+      languageOptions.forEach((option) => {
+        option.classList.add("active");
+      });
+    }
   }
 
-  function closeSelect(elem) {
-    languageOptions.forEach((option) => {
-      option.classList.remove("active");
-      elem.classList.add("active");
-      languageButton.classList.remove("active");
-    });
+  function closeSelect(elem, isMobile) {
+    if (isMobile) {
+      languageOptionsMobile.forEach((option) => {
+        option.classList.remove("active");
+        languageButtonMobile.classList.remove("active");
+      });
+    } else {
+      languageOptions.forEach((option) => {
+        option.classList.remove("active");
+        elem.classList.add("active");
+        languageButton.classList.remove("active");
+      });
+    }
   }
 
   languageButton.addEventListener("click", () => {
-    openSelect();
+    const isMobile = false;
+
+    openSelect(isMobile);
+  });
+
+  languageButtonMobile.addEventListener("click", () => {
+    const isMobile = true;
+
+    openSelect(isMobile);
   });
 
   languageOptions.forEach((option) => {
     option.addEventListener("click", (event) => {
       if (languageButton.classList.contains("active")) {
+        const isMobile = false;
+
         event.stopPropagation();
-        closeSelect(event.target);
+        closeSelect(event.target, isMobile);
+      }
+    });
+  });
+
+  languageOptionsMobile.forEach((option) => {
+    option.addEventListener("click", (event) => {
+      if (languageButtonMobile.classList.contains("active")) {
+        const isMobile = true;
+
+        event.stopPropagation();
+        closeSelect(event.target, isMobile);
       }
     });
   });
